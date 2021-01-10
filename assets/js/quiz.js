45ddd30e2a427a73bc715f629a0ede9b382621e5
Harry-Leepz <hdhillon478@gmail.com>
2021-01-10 11:55:13 +0000

    Add eventlistener function to update the selected answer with correct / incorrect css class in response to user answer selection. Trial and error attempts to try to fix dev tools console errors in quiz.html and quiz.js.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,99 +1,99 @@
 // --- Global Variables --- //
 
 const questionText = document.querySelector(`#question`);
-const answerText = Array.from(document.querySelectorAll(`.answer-content`));
+const answerText = Array.from(document.querySelectorAll(`.choice-content`));
 const timerText = document.querySelector(`#quiz-timer`);
 const scoreText = document.querySelector(`#score`);
 
 let currentQuestion = {}
-let acceptingAnswer = true
+let acceptingchoice = true
 let score = 0
 let questionCounter = 0
 let availableQuestions = []
 
 
 
-// --- Quiz Questions and Answers --- //
+// --- Quiz Questions and choices --- //
 
 let questions = [
     {
         question: 'Who created the Rule of two for the Sith ?',
-        answer1: 'Darth Sidius',
-        answer2: 'Darth Tyrannus',
-        answer3: 'Darth Plagueis',
-        answer4: 'Darth Bane',
+        choice1: 'Darth Sidius',
+        choice2: 'Darth Tyrannus',
+        choice3: 'Darth Plagueis',
+        choice4: 'Darth Bane',
         answer: 4,
     },
     {
         question: "What is the name of of the Jedi Master who served on the Jedi Council and was the same species as Master Yoda?",
-        answer1: "Master Yodel",
-        answer2: "Master Yaddle",
-        answer3: "Master Yokun",
-        answer4: "Master Yaddell",
+        choice1: "Master Yodel",
+        choice2: "Master Yaddle",
+        choice3: "Master Yokun",
+        choice4: "Master Yaddell",
         answer: 2,
     },
     {
         question: "What was the nickname given to Ahsoka Tano by her Master Anakin Skywalker?",
-        answer1: "Snips",
-        answer2: "Crips",
-        answer3: "Trips",
-        answer4: "Snoks",
+        choice1: "Snips",
+        choice2: "Crips",
+        choice3: "Trips",
+        choice4: "Snoks",
         answer: 1,
     },
     {
         question: "What species does Ahsoka Tano belong to?",
-        answer1: "Utapaun",
-        answer2: "Chagrian",
-        answer3: "Arcona",
-        answer4: "Togruta",
+        choice1: "Utapaun",
+        choice2: "Chagrian",
+        choice3: "Arcona",
+        choice4: "Togruta",
         answer: 4,
     },
     {
         question: "In what year did Master Yoda become one with the Force?",
-        answer1: "896 BBY",
-        answer2: "2 ABY",
-        answer3: "4 ABY",
-        answer4: "1 ABY",
+        choice1: "896 BBY",
+        choice2: "2 ABY",
+        choice3: "4 ABY",
+        choice4: "1 ABY",
         answer: 3,
     },
     {
         question: "What is the name of the Jedi Master who shared the same species as Ahsoka Tano?",
-        answer1: "Jedi Master Asaak Dan",
-        answer2: "Jedi Master Kirames Kaj",
-        answer3: "Jedi Master Shaak Ti",
-        answer4: "Jedi Master Raana Tey",
+        choice1: "Jedi Master Asaak Dan",
+        choice2: "Jedi Master Kirames Kaj",
+        choice3: "Jedi Master Shaak Ti",
+        choice4: "Jedi Master Raana Tey",
         answer: 3,
     },
     {
         question: "What is the name of the Convor that follows Ahsoka Tano?",
-        answer1: "Morai",
-        answer2: "Moray",
-        answer3: "Mortis",
-        answer4: "Mor'Tuun",
+        choice1: "Morai",
+        choice2: "Moray",
+        choice3: "Mortis",
+        choice4: "Mor'Tuun",
         answer: 1,
     },
     {
         question: "What color was Obi-Wan Kenobi's lightsaber",
-        answer1: "Red",
-        answer2: "Green",
-        answer3: "Blue",
-        answer4: "Purple",
+        choice1: "Red",
+        choice2: "Green",
+        choice3: "Blue",
+        choice4: "Purple",
         answer: 3,
     },
     {
         question: "Which former smuggler and pirate runs an interstellar tavern in a castle on the fictional planet Takodana, and is over 1000 years old?",
-        answer1: "Hondo Ohnaka",
-        answer2: "Enfys Nest",
-        answer3: "Cikatro Vizago",
-        answer4: "Maz Kanata",
+        choice1: "Hondo Ohnaka",
+        choice2: "Enfys Nest",
+        choice3: "Cikatro Vizago",
+        choice4: "Maz Kanata",
         answer: 4,
     },
     {
         question: "Which Sith lord had the ability to drain entire planets by using their connection to the Force?",
-        answer1: "Darth Bane",
-        answer2: "Darth Nihilus",
-        answer3: "Darth Malgus",
-        answer4: "Darth Vitiate",
+        choice1: "Darth Bane",
+        choice2: "Darth Nihilus",
+        choice3: "Darth Malgus",
+        choice4: "Darth Vitiate",
         answer: 2,
     }
 ]
@@ -123,12 +123,43 @@ getNewQuestion = () => {
     currentQuestion = availableQuestions[questionsIndex]
     question.innerText = currentQuestion.question
 
-    answerText.forEach(answer => {
-        const number = answer.dataset['number']
-        answer.innerText = currentQuestion['answer' + number]
+    choiceText.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
     })
 
     availableQuestions.splice(questionsIndex, 1)
 
-    acceptingAnswer = true
+    acceptingchoice = true
 }
+
+answerText.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptinganswer) return
+
+        acceptingchoice = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(scorePoints)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedchoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startQuiz()
