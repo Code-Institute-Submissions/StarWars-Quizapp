ace00abd2454a77ff2a5f09a59f37d2586dbffea
Harry-Leepz <hdhillon478@gmail.com>
2021-01-10 10:37:25 +0000

    Add Quiz questions and answers in an array in quiz.js.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -12,3 +12,85 @@ let availableQuestions = []
 
 // --- Quiz Questions --- //
 
+let questions = [
+    {
+        question: 'Who created the Rule of two for the Sith ?',
+        choice1: 'Darth Sidius',
+        choice2: 'Darth Tyrannus',
+        choice3: 'Darth Plagueis',
+        choice4: 'Darth Bane',
+        answer: 4,
+    },
+    {
+        question: "What is the name of of the Jedi Master who served on the Jedi Council and was the same species as Master Yoda?",
+        choice1: "Master Yodel",
+        choice2: "Master Yaddle",
+        choice3: "Master Yokun",
+        choice4: "Master Yaddell",
+        answer: 2,
+    },
+    {
+        question: "What was the nickname given to Ahsoka Tano by her Master Anakin Skywalker?",
+        choice1: "Snips",
+        choice2: "Crips",
+        choice3: "Trips",
+        choice4: "Snoks",
+        answer: 1,
+    },
+    {
+        question: "What species does Ahsoka Tano belong to?",
+        choice1: "Utapaun",
+        choice2: "Chagrian",
+        choice3: "Arcona",
+        choice4: "Togruta",
+        answer: 4,
+    },
+    {
+        question: "In what year did Master Yoda become one with the Force?",
+        choice1: "896 BBY",
+        choice2: "2 ABY",
+        choice3: "4 ABY",
+        choice4: "1 ABY",
+        answer: 3,
+    },
+    {
+        question: "What is the name of the Jedi Master who shared the same species as Ahsoka Tano?",
+        choice1: "Jedi Master Asaak Dan",
+        choice2: "Jedi Master Kirames Kaj",
+        choice3: "Jedi Master Shaak Ti",
+        choice4: "Jedi Master Raana Tey",
+        answer: 3,
+    },
+    {
+        question: "What is the name of the Convor that follows Ahsoka Tano?",
+        choice1: "Morai",
+        choice2: "Moray",
+        choice3: "Mortis",
+        choice4: "Mor'Tuun",
+        answer: 1,
+    },
+    {
+        question: "What color was Obi-Wan Kenobi's lightsaber",
+        choice1: "Red",
+        choice2: "Green",
+        choice3: "Blue",
+        choice4: "Purple",
+        answer: 3,
+    },
+    {
+        question: "Which former smuggler and pirate runs an interstellar tavern in a castle on the fictional planet Takodana, and is over 1000 years old?",
+        choice1: "Hondo Ohnaka",
+        choice2: "Enfys Nest",
+        choice3: "Cikatro Vizago",
+        choice4: "Maz Kanata",
+        answer: 4,
+    },
+    {
+        question: "Which Sith lord had the ability to drain entire planets by using their connection to the Force?",
+        choice1: "Darth Bane",
+        choice2: "Darth Nihilus",
+        choice3: "Darth Malgus",
+        choice4: "Darth Vitiate",
+        answer: 2,
+    }
+]
