a6a12032d3a89e16a7180319c451b7b9e528309a
Harry-Leepz <hdhillon478@gmail.com>
2021-01-10 09:31:00 +0000

    Add globals variables section to quiz.js. Variables created to target different objects for the quiz.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,1 +1,14 @@
-console.log(`Hello World!`);
+// --- Global Variables --- //
+
+const questionText = document.querySelector(`#question`);
+const answerText = Array.from(document.querySelectorAll(`.answer-content`));
+const timerText = document.querySelector(`#quiz-timer`);
+const scoreText = document.querySelector(`#score`);
+
+let currentQuestion = 0
+let acceptingAnswer = true
+let score = 0
+let availableQuestions = []
+
+// --- Quiz Questions --- //
+
