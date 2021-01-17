24697f4ea5be60ab8d2b3802cb6ee2d2ddb0220e
Harry-Leepz <hdhillon478@gmail.com>
2021-01-17 15:47:43 +0000

    Add global variables to quiz-end.js.

---
+++ assets/js/quiz-end.js
@@ -0,0 +1,6 @@
+// --- Global Variables --- //
+const username = document.querySelector(`#username`);
+const saveScore = document.querySelector(`#save-score`);
+const finalScore = document.querySelector(`#quiz-finalscore`);
+const recentScore = document.querySelector(`#recent-score`);
+
