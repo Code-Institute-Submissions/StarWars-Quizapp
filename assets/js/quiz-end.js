8d54fba9912eaeac83b4220693349ffc72cc5162
Harry-Leepz <hdhillon478@gmail.com>
2021-01-17 16:15:18 +0000

    Add quiz-end.js file path to quiz-end.html in scripts section at the bottom of the page. Add event in quiz-end.js to save score and username in local storage. Add event listener to allow user to save username once text has been put into input box.

--- assets/js/quiz-end.js
+++ assets/js/quiz-end.js
@@ -4,3 +4,35 @@ const saveScore = document.querySelector(`#save-score`);
 const finalScore = document.querySelector(`#quiz-finalscore`);
 const recentScore = document.querySelector(`#recent-score`);
 
+
+const highScores = JSON.parse(localStorage.getItem(`highScores`)) || []
+
+const maxHighScores = 6
+
+finalScore.innerText = recentScore
+
+username.addEventListener(`keyup`, () => {
+    saveScore.disabled = !username.value
+})
+
+saveHighscore = e => {
+    e.preventDefault()
+
+    const score = {
+        score: recentScore,
+        name: username.value
+    }
+
+    highScores.push(score)
+
+    highScores.sort((a,b) => {
+        return b.score - a.score
+    })
+
+    highScores.splice(6)
+
+    localStorage.setItem(`highScores`, JSON.stringify(highScores))
+    window.location.assign(`/`)
+
+
+}
