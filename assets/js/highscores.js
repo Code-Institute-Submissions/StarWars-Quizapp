732cfe01f2f7fe15cfd6f8804c32d45d53c4d0a9
Harry-Leepz <hdhillon478@gmail.com>
2021-01-25 13:05:49 +0000

    Use code beautifier on highscores.js and add comment to explain function.

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,9 +1,8 @@
 /*jshint esversion: 6 */
-
 const leaderboard = document.querySelector(`#leaderboard`);
 const highScores = JSON.parse(localStorage.getItem(`highScores`)) || [];
-
-leaderboard.innerHTML = 
-highScores.map(score => {
-    return `<li class="list-content">${score.name} - ${score.score}</li>`;
-}).join(``);
+//Create a <li> element that acts as a leaderboard with the Users saved name and score.
+leaderboard.innerHTML =
+    highScores.map(score => {
+        return `<li class="list-content">${score.name} - ${score.score}</li>`;
+    }).join(``);
